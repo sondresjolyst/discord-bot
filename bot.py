1a6d81cadc4531360f81b1023e49d9ea77b5cfa3
sondresjolyst <sonyslyst@gmail.com>
2022-08-21 11:01:18 +0200

    removed func

--- bot.py
+++ bot.py
@@ -14,15 +14,10 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="Skjævelandsbrunå", url="https://www.google.com"))
 
-def load_extensions():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
-    else:
-      print(f'Unable to load {filename[:-3]}')
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+else:
+    print(f'Unable to load {filename[:-3]}')
 
-def main():
-    load_extensions()
-    bot.run(token)
-
-main()
+bot.run(token)
